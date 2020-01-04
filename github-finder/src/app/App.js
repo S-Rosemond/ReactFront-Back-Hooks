a9a62846b9af9ce611e8c7abf0883bf168d78887
Samuel <js.rosemond@gmail.com>
2020-01-04 13:51:59 -0500

    minor update: pagination logic

--- github-finder/src/app/App.js
+++ github-finder/src/app/App.js
@@ -1,46 +1,47 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 import Navbar from '../components/Layout/NavBar';
 import Users from '../components/users/Users';
-import Pagination from '../components/utils/Pagination'
-
+import Pagination from '../components/utils/Pagination';
 
 class App extends Component {
-state = {
-  users: [],
-  loading: false,
-  currentPage: 1,
-  postPerPage: 9
-}
-async componentDidMount() {
-  this.setState({loading: true})
- 
-  const data = await this.fetchUsers()
+	state = {
+		users: [],
+		loading: false,
+		currentPage: 1,
+		postPerPage: 6
+	};
+	async componentDidMount() {
+		this.setState({ loading: true });
 
-  this.setState({users: data, loading: false})
-}
-async fetchUsers() {
-  const res = await axios.get('https://api.github.com/users')
+		const data = await this.fetchUsers();
 
-  return res.data
-}
-paginate =(number) => {
-  this.setState({currentPage:number})
-}
+		this.setState({ users: data, loading: false });
+	}
+	async fetchUsers() {
+		const res = await axios.get('https://api.github.com/users');
+
+		return res.data;
+	}
+	paginate = (number) => {
+		this.setState({ currentPage: number });
+	};
 
 	render() {
-    const indexOfLastPost = this.state.currentPage * this.state.postPerPage; 
-    const indexOfFirstPost = indexOfLastPost - this.state.postPerPage 
-    const currentPosts = this.state.users.slice(indexOfFirstPost, indexOfLastPost) 
+		const indexOfLastPost = this.state.currentPage * this.state.postPerPage;
+		const indexOfFirstPost = indexOfLastPost - this.state.postPerPage;
+		const currentPosts = this.state.users.slice(indexOfFirstPost, indexOfLastPost);
 
 		return (
 			<div className="App">
 				<Navbar />
 				<div className="container">
 					<Users users={currentPosts} />
-          <Pagination postPerPage={this.state.postPerPage} totalPosts={this.state.users.length}
-          paginate={this.paginate}
-          />
+					<Pagination
+						postPerPage={this.state.postPerPage}
+						totalPosts={this.state.users.length}
+						paginate={this.paginate}
+					/>
 				</div>
 			</div>
 		);
--- github-finder/src/components/utils/Pagination.js
+++ github-finder/src/components/utils/Pagination.js
@@ -1,32 +1,25 @@
-import  React from 'react';
+import React from 'react';
 
-const Pagination = ({postPerPage, totalPosts, paginate}) => {
-    const pageNumbers = []
+const Pagination = ({ postPerPage, totalPosts, paginate }) => {
+	const pageNumbers = [];
 
-    for(let i = 1; i < Math.ceil(totalPosts/postPerPage); ++i) {
-        pageNumbers.push(i)
-    }
+	for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); ++i) {
+		pageNumbers.push(i);
+	}
 
-    return (
-        <div>
-            <ul className='grid-3 text-center' >
-                {
-                    pageNumbers.map(number =>(
-                        <li key={number} className='btn-mx bg-primary '>
-                        <a href="!#" 
-                        className='text-light flex'
-                        onClick={() => paginate(number)
-                            
-                        }>
-                            {number}
-                        </a>
-                        </li>
-                    ) )
-                }
-            </ul>
-        </div>
-    )
-}
-
+	return (
+		<div>
+			<ul className="grid-3 text-center">
+				{pageNumbers.map((number) => (
+					<li key={number} className="btn-mx bg-primary ">
+						<a href="!#" className="text-light flex" onClick={() => paginate(number)}>
+							{number}
+						</a>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 export default Pagination;
