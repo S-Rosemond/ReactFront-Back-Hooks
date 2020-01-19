89251ed5697c1557cb3669b797854297f8d51ab5
Samuel <js.rosemond@gmail.com>
2020-01-18 19:55:09 -0500

    Class section done, hook conversion next

--- github-finder/src/app/App.js
+++ github-finder/src/app/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from '../components/Layout/NavBar';
 import Users from '../components/users/Users';
 import Search from './../components/users/Search';
 import Alert from './../components/utils/Alert';
 import Pagination from '../components/utils/Pagination';
 import About from './../components/pages/About';
-import  User  from './../components/users/User';
+import User from './../components/users/User';
 
 class App extends Component {
 	state = {
@@ -30,7 +30,7 @@ class App extends Component {
 		this.setState({ loading: true });
 
 		const res = await axios.get(endPoint);
-		
+
 		return res;
 	}
 	paginate = (number) => {
@@ -57,58 +57,64 @@ class App extends Component {
 
 	// Get single github user
 	getUser = async (username) => {
-		const endPoint = `https://api.github.com/users/${username}?client_id=${process.env
-			.REACT_APP_GITHUB_CLIENT_ID & process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
+		const endPoint = `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID &
+			process.env.REACT_APP_GITHUB_CLIENT_SECRET}`;
 
-			const res = await this.fetchUsers(endPoint)
-			
-			this.setState({user: res.data, loading: false})
-		
-	}
-	
+		const res = await this.fetchUsers(endPoint);
+
+		this.setState({ user: res.data, loading: false });
+	};
 
 	render() {
 		let indexOfLastPost = this.state.currentPage * this.state.postPerPage;
 		let indexOfFirstPost = indexOfLastPost - this.state.postPerPage;
-		 let currentPosts = this.state.users.slice(indexOfFirstPost, indexOfLastPost);
+		let currentPosts = this.state.users.slice(indexOfFirstPost, indexOfLastPost);
 		let loading = this.state.loading;
 
 		return (
-			
 			<Router>
-			<div className="App">
-				<Navbar />
-				
-			</div>
-			<Switch>
-				<Route exact path='/' render={(props) => (<div className="container">
-					<Alert alert={this.state.alert} />
-					<Search
-						searchUsers={this.searchUsers}
-						clearUsers={this.clearUsers}
-						showClear={this.state.users.length > 0 ? true : false}
-						setAlert={this.setAlert}
+				<div className="App">
+					<Navbar />
+				</div>
+				<Switch>
+					<Route
+						exact
+						path="/"
+						render={(props) => (
+							<div className="container">
+								<Alert alert={this.state.alert} />
+								<Search
+									searchUsers={this.searchUsers}
+									clearUsers={this.clearUsers}
+									showClear={this.state.users.length > 0 ? true : false}
+									setAlert={this.setAlert}
+								/>
+								<Users users={currentPosts} loading={loading} />
+								<Pagination
+									postPerPage={this.state.postPerPage}
+									totalPosts={this.state.users.length}
+									paginate={this.paginate}
+								/>
+							</div>
+						)}
 					/>
-					<Users users={currentPosts} loading={loading} />
-					<Pagination
-						postPerPage={this.state.postPerPage}
-						totalPosts={this.state.users.length}
-						paginate={this.paginate}
-					/>
-				</div>)
-				} />
-				<Route exact path='/about' component={About} />
-				<Route exact path='/user/:login' render={(props) => (
-					<User {...props} getUser={this.getUser} user={this.state.user} loading={this.state.loading}/> 
-				)
-					
-				
-				} />
-
-			</Switch>
-
+					<div className="container">
+						<Route exact path="/about" component={About} />
+						<Route
+							exact
+							path="/user/:login"
+							render={(props) => (
+								<User
+									{...props}
+									getUser={this.getUser}
+									user={this.state.user}
+									loading={this.state.loading}
+								/>
+							)}
+						/>
+					</div>
+				</Switch>
 			</Router>
-			
 		);
 	}
 }
--- github-finder/src/components/users/User.js
+++ github-finder/src/components/users/User.js
@@ -1,20 +1,78 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import Spinner from '../Layout/Spinner';
+import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
 
- class User extends Component {
-    componentDidMount() {
-        this.props.getUser(this.props.match.params.login)
+class User extends Component {
+	componentDidMount() {
+		this.props.getUser(this.props.match.params.login);
+	}
+	render() {
+		const {
+			name,
+			avatar_url,
+			location,
+			bio,
+			blog,
+			company,
+			login,
+			html_url,
+			followers,
+			following,
+			public_repos,
+			public_gists,
+			hireable
+		} = this.props.user;
+		const { loading } = this.props;
 
-    }
-    render() {
-        const {name, avatar_url,location, bio, blog, login,html_url, followers, following, public_repos,public_gists,hireable} = this.props.user
-        const {loading} = this.props;
-        
-        return (
-            <div style={{color:'black'}}>
-                {name}
-            </div>
-        )
-    }
+		return loading ? (
+			<Spinner />
+		) : (
+			<Fragment>
+				<Link to="/" className="btn btn-dark">
+					Back To Search
+				</Link>
+
+				<div className="card grid-2 ">
+					<div className="all-center">
+						<img src={avatar_url} alt="" className="round-img" style={{ width: '150px' }} />
+
+						<h1>{name}</h1>
+						<h4>Location: {location}</h4>
+						{company && <h4>Company: {company}</h4>}
+						{blog && <h4>Website: {blog}</h4>}
+					</div>
+					<div className="">
+						{bio && (
+							<Fragment>
+								<h3 className="my-1">Bio</h3>
+								<p>{bio}</p>
+							</Fragment>
+						)}
+
+						<a href={html_url} className="btn btn-dark my-1 ">
+							Github profile
+						</a>
+
+						<div>
+							Hireable: {' '}
+							{hireable ? (
+								<i className="fas fa-check text-success" />
+							) : (
+								<i className="fas fa-times-circle text-danger" />
+							)}
+						</div>
+					</div>
+				</div>
+			</Fragment>
+		);
+	}
 }
 
-export default User
+User.propTypes = {
+	loading: PropTypes.bool.isRequired,
+	user: PropTypes.object.isRequired,
+	getUser: PropTypes.func.isRequired
+};
+
+export default User;
